9517e1778cadf74c876e41a7dd9a2d941cd9b388
Sakib Mahmood Dhrubo <sakib.dhrubo@outlook.com>
2020-03-09 18:00:55 +0600

    55

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -125,7 +125,7 @@ namespace Practice
                         {
                             sum = sum - value;
                         }
-
+                            
                     }
                 }
 
